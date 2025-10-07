ed9c1ab4896f0d93611322f0f7f044fbd605a80c
meshahzad92 <mshahzadwaris92@gmail.com>
2025-10-07 14:18:03 +0500

    Hangup tool integrated.(5)

--- main.py
+++ main.py
@@ -58,7 +58,13 @@ ULTRAVOX_CALL_CONFIG = {
         },
         {
             "toolName": "hangUp"  
+        },
+         "inactivityMessages": [
+        {
+            "duration": "5s", 
+            "message": "I didn't hear a selection. Let's try it this way: Press 1 for Sales and Partnerships, Press 2 for VIVA Audio Bible, Press 3 for Casting and Talent, Press 4 for Press and Media, Press 5 for Technical Support, Or press 0 to leave a message."
         }
+    ]
     ]
 }
 
